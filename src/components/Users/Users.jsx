3026d62a7adad30ea058f69cfc85e9aa2fb21c03
Kalips0o <Kalipsoo92@gmail.com>
2022-05-21 22:18:24 +0300

    51 Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,15 @@
 import React from 'react';
 import styles from './users.module.css'
+import axios from "axios";
+import userPhoto from '../../assets/img/206853.png'
 
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://cdnn21.img.ria.ru/images/56176/54/561765466_0:0:0:0_600x0_80_0_0_9ba3dad3909fce093d3605e022c32b21.jpg',
-                followerd: false,
-                fullName: 'Stiv Jobs',
-                status: 'I am a boss',
-                location: {city: 'New York', country: 'America'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://i.pinimg.com/736x/b4/72/a1/b472a1234f0fad2a6bd45750c14e7bec.jpg',
-                followerd: false,
-                fullName: 'Hanna Berezko',
-                status: 'Hello, I am Hanna',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://www.graycell.ru/picture/big/gloriya.jpg',
-                followerd: true,
-                fullName: 'Hloria Lucky',
-                status: 'Skilled herbologist and terrarium owner',
-                location: {city: 'Antananarivo', country: 'Madagaskar'}
-            },
-            {
-                id: 4,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Alex_Sparrow_04-11_RN_MOW.jpg',
-                followerd: false,
-                fullName: 'Alexey Vorobey',
-                status: 'Fitness Trainer',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 5,
-                photoUrl: 'https://assets.gq.ru/photos/5d9f4dcf55f8500008a802d9/4:3/w_800,h_600,c_limit/0.jpg',
-                followerd: false,
-                fullName: 'Jack Sparrow',
-                status: 'Kaptain,Jack Sparrow ',
-                location: {city: 'Tortuga', country: ' Caribe'}
-            }])
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        })
     }
     return (
         <div>
@@ -52,7 +17,8 @@ const Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={styles.userPhoto}/>
+                            <img src={u.photos.small !== null ? u.photos.small : userPhoto}
+                                 className={styles.userPhoto}/>
                         </div>
                         <div>
                             {u.followerd ?
@@ -68,12 +34,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.Name}</div>
                             <div> {u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country} </div>
-                            <div>{u.location.city} </div>
+                            <div>{'u.location.country'} </div>
+                            <div>{'u.location.city'} </div>
 
 
                         </span>
