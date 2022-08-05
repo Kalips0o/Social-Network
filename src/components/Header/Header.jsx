fa361c288e142550a3483af5ad51180047c31583
Kalips0o <Kalipsoo92@gmail.com>
2022-08-05 19:20:21 +0300

    78  login и logout api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,9 @@ const Header = (props) => {
             src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/RPC-JP_Logo.png/600px-RPC-JP_Logo.png'/>
 
         <div className={s.loginBlock}>
-            {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            {props.isAuth ?
+                <div>{props.login} - <button onClick={props.logout}> Log out </button></div>
+                : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
 }
