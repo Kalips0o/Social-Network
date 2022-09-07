21b12108cad959807b84f266cd432f06005ec624
Kalips0o <Kalipsoo92@gmail.com>
2022-09-07 12:43:20 +0300

    96 image upload, shouldComponentUpdate fix

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,32 +1,45 @@
 import React from 'react';
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {getStatus, getUserProfile, updateStatus} from "../../redux/profile-reducer";
+import {getStatus, getUserProfile, savePhoto, updateStatus} from "../../redux/profile-reducer";
 import {withRouter} from "react-router-dom";
 import {compose} from "redux";
 
 
 class ProfileContainer extends React.Component {
-
-    componentDidMount() {
+    refreshProfile() {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = this.props.authorizedUserId;
             if (!userId) {
                 this.props.history.push("/login")
             }
-
         }
         this.props.getUserProfile(userId)
         this.props.getStatus(userId)
     }
 
 
-    render() {
+    componentDidMount() {
+        this.refreshProfile()
+    }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.match.params.userId != prevProps.match.params.userId) {
+            this.refreshProfile()
+        }
+    }
+
+
+    render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status}
+            <Profile {...this.props}
+                     savePhoto={this.props.savePhoto}
+                     isOwner={!this.props.match.params.userId}
+                     profile={this.props.profile}
+                     status={this.props.status}
                      updateStatus={this.props.updateStatus}/>
+
         )
     }
 }
@@ -41,7 +54,7 @@ let mapStateToProps = (state) => ({
 
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus, savePhoto}),
     withRouter,
     // withAuthRedirect
 )
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,36 +2,48 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloader/Preloader";
 import ProfileStatusWithHooks from "../ProfileStatusWithHooks";
+import userPhoto from "../../../assets/img/206853.png"
 
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
     if (!profile) {
         return <Preloader/>
     }
+
+
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto(e.target.files[0])
+        }
+    }
+
+
     return <div>
 
+        <div>
+            <img className={s.userPhotoProfile} src={profile.photos.large || userPhoto}/>
+            {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
+        </div>
 
         <div className={s.descriptionBlock}>
+
             <div>
                 Имя: {profile.fullName}
             </div>
-            <div>
-                <img src={profile.photos.large}/>
-            </div>
+
             <div>
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
 
 
-            <div>
-                Девиз: {profile.aboutMe}
-            </div>
-            <div>
-                <div> instagram: {profile.contacts.instagram} </div>
-                <div> vk: {profile.contacts.vk} </div>
-                <div> twitter: {profile.contacts.twitter} </div>
-
-            </div>
+            {/*<div>*/}
+            {/*    Девиз: {profile.aboutMe}*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <div> instagram: {profile.contacts.instagram} </div>*/}
+            {/*    <div> vk: {profile.contacts.vk} </div>*/}
+            {/*    <div> twitter: {profile.contacts.twitter} </div>*/}
+            {/*</div>*/}
         </div>
     </div>
 
