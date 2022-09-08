afa2bd1d3afa31f309b637ac1dcc5e5d678c6773
Kalips0o <Kalipsoo92@gmail.com>
2022-09-08 14:34:58 +0300

    Icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import s from './Navbar.module.css'
+import s from './Navbar.module.css';
+import MessagesIco from './../../assets/img/Icons/message.png';
+import ProfileIco from './../../assets/img/Icons/profile.png';
+import NewsIco from './../../assets/img/Icons/news.png';
+import MusicIco from './../../assets/img/Icons/music.png';
+import SettingIco from './../../assets/img/Icons/setting.png';
+import UsersIco from './../../assets/img/Icons/allUsers.png';
+import FriendsIco from './../../assets/img/Icons/friends.png';
+
 
 const NavBar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <NavLink to="/profile" activeClassName={s.activeLink}>Profile </NavLink>
+            <NavLink to="/profile" activeClassName={s.activeLink}><img src={ProfileIco}/> Profile </NavLink>
+        </div>
+
+        <div className={s.item}>
+            <NavLink to='/Dialogs' activeClassName={s.activeLink}><img src={MessagesIco}/> Messages </NavLink>
         </div>
         <div className={s.item}>
-            <NavLink to='/Dialogs' activeClassName={s.activeLink}> Messages </NavLink>
+            <NavLink to='/News' activeClassName={s.activeLink}><img src={NewsIco}/> News </NavLink>
         </div>
         <div className={s.item}>
-            <NavLink to='/News' activeClassName={s.activeLink}> News </NavLink>
+            <NavLink to='/Music' activeClassName={s.activeLink}> <img src={MusicIco}/> Music </NavLink>
         </div>
         <div className={s.item}>
-            <NavLink to='/Music' activeClassName={s.activeLink}> Music </NavLink>
+            <NavLink to='/Setting' activeClassName={s.activeLink}> <img src={SettingIco}/> Setting </NavLink>
         </div>
         <div className={s.item}>
-            <NavLink to='/Setting' activeClassName={s.activeLink}> Setting </NavLink>
+            <NavLink to='/Users' activeClassName={s.activeLink}><img src={UsersIco}/> Users </NavLink>
         </div>
         <div className={s.item}>
-            <NavLink to='/Users' activeClassName={s.activeLink}> Users </NavLink>
+            <NavLink to='/Friends' activeClassName={s.activeLink}><img src={FriendsIco}/> Friends </NavLink>
         </div>
     </nav>
 }
