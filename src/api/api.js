602594c1eb165066533efe9ac1a254580c1df307
Kalips0o <Kalipsoo92@gmail.com>
2022-09-14 13:14:38 +0300

    Revert "Api"
    This reverts commit 36faa94ed9aca83fca3a5c566a8883945fa83b27.

--- src/api/api.js
+++ src/api/api.js
@@ -3,12 +3,11 @@ import axios from "axios";
 
 const instance = axios.create({
     withCredentials: true,
-    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        "API-KEY": "b2224b5f-5c54-4a8d-a10b-c3a74d42aafe"
+        "API-KEY": "bf527952-90e1-4dc2-a751-8d0fb4bf8491"
     }
 });
-
 
 
 export const usersAPI = {
@@ -18,53 +17,52 @@ export const usersAPI = {
                 return response.data;
             });
     },
+
     follow(userId) {
         return instance.post(`follow/${userId}`)
     },
+
     unfollow(userId) {
         return instance.delete(`follow/${userId}`)
-    },
-    getProfile(userId) {
-        console.warn('Obsolete method. Please profileAPI object.')
-        return profileAPI.getProfile(userId);
     }
 }
 
+
 export const profileAPI = {
     getProfile(userId) {
-        return instance.get(`profile/` + userId);
+        return instance.get(`profile/` + userId)
     },
     getStatus(userId) {
-        return instance.get(`profile/status/` + userId);
+        return instance.get(`profile/status/` + userId)
     },
     updateStatus(status) {
-        return instance.put(`profile/status`, {status: status});
+        return instance.put(`profile/status/`, {status: status})
     },
     savePhoto(photoFile) {
         const formData = new FormData();
-        formData.append("image", photoFile);
-
-        return instance.put(`profile/photo`, formData, {
+        formData.append("image", photoFile)
+        return instance.put(`profile/photo/`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
-        });
+        })
     },
     saveProfile(profile) {
         return instance.put(`profile`, profile);
     }
+
 }
+
 
 export const authAPI = {
     me() {
-        return instance.get(`auth/me`);
+        return instance.get(`auth/me`)
     },
     login(email, password, rememberMe = false) {
-        return instance.post(`auth/login`, {email, password, rememberMe});
+        return instance.post(`auth/login`, {email, password, rememberMe})
     },
     logout() {
-        return instance.delete(`auth/login`);
-    }
+        return instance.delete(`auth/login`)
+    },
+
 }
-
-
