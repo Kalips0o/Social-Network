3031ee02d0ccc8f6ac747c18e995b8b1c9488d18
Kalips0o <Kalipsoo92@gmail.com>
2022-12-07 19:39:03 +0300

    2.05 - React + connect, TypeScript, mapStateToProps, mapDispatchToProps / React JS

---
+++ src/components/Dialogs/Message/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './Message.module.css';
+import avatar from './../../../assets/img/UserPhoto/user.webp'
+
+export const Message = (props) => {
+    const messageClassName = `${s.message} ${props.name === "Me" ? s.outgoing : s.incoming}`;
+    const avatarSrc = props.name === "Me" ? avatar : avatar;
+
+    return (
+        <div className={messageClassName}>
+            <div className={s.senderInfo}>
+                <img className={avatar} src={avatarSrc} alt={props.name + " avatar"}/>
+                <p className={s.senderName}>{props.name}</p>
+            </div>
+            <div className={s.messageContent}>
+                <p className={s.message}>{props.message}</p>
+                <span className={s.messageTime}>{props.messageTime}</span>
+            </div>
+        </div>
+    );
+};
+
+
+export default Message;
